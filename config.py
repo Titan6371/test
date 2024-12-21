682139fb86e2c5bfcfeaa2089a6a1121403c6b42
jayop1089 <instagrampassrecovey+10@outlook.com>
2024-12-21 19:18:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-BOT_TOKEN = "7788865701:AAEHVM8BGHCY_fIECUSn_ydJ_45Jn0p72-M"
-ADMIN_IDS = ["7163028849"]
-GROUP_ID = -1002421977380  # Replace with your group ID
-GROUP_LINK = "https://t.me/+8Uy26VLnS3A2ZTg1"  # Replace with your group invite link
+BOT_TOKEN = "7315607724:AAEm4UPL-RnDwHM9pxqqHRB8SXcUtB36VbA"
+ADMIN_IDS = ["8163493962"]
+GROUP_ID = -1002310632208  # Replace with your group ID
+GROUP_LINK = "https://t.me/+CaiKLZ5iIjg5M2Q9"  # Replace with your group invite link
 # Default number of threads for attacks
 DEFAULT_THREADS = 800  # Set the default number of threads to use for attacks
